9048832d30fb0f7f538b9fc8ed5168590b274604
WaterSpooner <max@the-spooners.co.uk>
2023-12-07 21:34:52 +0000

    refactored day 7

--- day7/d7 pt2.py
+++ day7/d7 pt2.py
@@ -1,82 +1,33 @@
-import re
-from collections import Counter
-cardValue = {'A':13,'K':12,'Q':11,'J':1,'T':10,'9':9,'8':8,'7':7,'6':6,'5':5,'4':4,'3':3,'2':2}    
-def greatestHand(hand,amount,secondAmount = -1):
-    three = False
-    two = False
-    pairs = 0
-    jokerCount = hand.get('J',0)
+from collections import Counter 
+def cardValue(card):
+    value = {'A':13,'K':12,'Q':11,'J':1,'T':10,'9':9,'8':8,'7':7,'6':6,'5':5,'4':4,'3':3,'2':2}
+    score = sum([value[card[i]]*14**(5-i) for i,e in enumerate(card)])
+    return score
 
-    if secondAmount == 2:
-        for card in hand.keys():
-            if hand[card] == 2:
-                pairs +=1
-        if pairs == 2:
-            return True
-        return False
-    
-    if secondAmount == 3:
-        if jokerCount == 1:
-            for card in hand.keys():
-                if hand[card] == 2 and card != 'J':
-                    pairs +=1
-            if pairs == 2:
-                return True
-            return False
-        else:
-            for card in hand.keys():
-                if hand[card] == 2:
-                    two = True
-                if hand[card] == 3:
-                    three = True
-            return two and three
-
-    for card in hand.keys():
-        if card == 'J':
-            if hand[card] == amount:
-                return True
-        else:
-            if hand[card]+jokerCount == amount:
-                return True        
-    return False
-
-def calculateValue(hand,handValue):
-    value = 0
-    handLen = len(hand)
-    for i in range(handLen):
-        value += (cardValue[hand[i]]) * (14**(5-i-1))
-    value += (handValue) * (14**(handLen))
-    return value
+def handScore(hand):
+    cards = Counter(hand[0])
+    cards['J'] = 0
+    del cards['J']
+    count = sorted(list(cards.values()))
+    if count == [5] or count == [] or len(count) == 1:
+        return (7,hand)
+    elif count == [1,4] or count == [1,1] or count == [1,3] or count == [1,2]:
+        return (6,hand)
+    elif count == [2,3] or count == [2,2]:
+        return (5,hand)
+    elif count == [1,1,3] or count == [1,1,1] or count == [1,1,2]:
+        return (4,hand)
+    elif count == [1,2,2]:
+        return (3,hand)
+    elif count == [1,1,1,2] or count == [1,1,1,1]:
+        return (2,hand)
+    elif count == [1,1,1,1,1]:
+        return (1,hand)
 
 file = open("day7/input.txt", "r")
 text = file.read()
-hands = {}
-for line in text.split('\n'):
-    l = line.split(' ')
-    hands[l[0]] = int(l[1]) 
-handValues ={}
-
-for hand in hands.keys():
-    cards ={}
-    for card in hand:
-        cards[card] = cards.get(card,0)+1
-    validCards = []
-    validCards.append(greatestHand(cards,5))
-    validCards.append(greatestHand(cards,4))
-    validCards.append(greatestHand(cards,2,3))
-    validCards.append(greatestHand(cards,3))
-    validCards.append(greatestHand(cards,2,2))
-    validCards.append(greatestHand(cards,2))
-    validCards.append(greatestHand(cards,1))
-    score = len(validCards) - validCards.index(True)
-    handValues[hand] = calculateValue(hand,score)
-
-i = len(hands)
-total = 0
-while i > 0:
-    hand = max(handValues, key=handValues.get)
-    print(i,hand,hands[hand])
-    total += hands[hand] * i
-    i-=1
-    handValues[hand] = -1
-print(total)
+hands = [line.split(" ") for line in text.split('\n')]
+hands = [handScore(hand) for hand in hands]
+hands = sorted(hands, key=lambda element: (element[0], cardValue(element[1][0])))
+score = sum([(i+1)*int(x[1][1]) for i,x in enumerate(hands)])
+print(score)
--- day7/d7.py
+++ day7/d7.py
@@ -1,54 +1,30 @@
-import re
-from collections import Counter
-cardValue = {'A':13,'K':12,'Q':11,'J':10,'T':9,'9':8,'8':7,'7':6,'6':5,'5':4,'4':3,'3':2,'2':1}    
-def greatestHand(hand,amount,twoPair=False):
-    pairCount = 0
-    for card in hand.keys():
-        if hand[card] == amount:
-            if not twoPair:
-                return True
-            pairCount +=1
-    if twoPair and pairCount == 2:
-        return True
-    return False
+from collections import Counter 
+def cardValue(card):
+    value = {'A':13,'K':12,'Q':11,'J':10,'T':9,'9':8,'8':7,'7':6,'6':5,'5':4,'4':3,'3':2,'2':1}
+    score = sum([value[card[i]]*14**(5-i) for i,e in enumerate(card)])
+    return score
 
-def calculateValue(hand,handValue):
-    value = 0
-    handLen = len(hand)
-    for i in range(handLen):
-        value += (cardValue[hand[i]]) * (14**(5-i-1))
-    value += (handValue) * (14**(handLen))
-    return value
+def handScore(hand):
+    count = sorted(list(Counter(hand[0]).values()))
+    if count == [5]:
+        return (7,hand)
+    elif count == [1,4]:
+        return (6,hand)
+    elif count == [2,3]:
+        return (5,hand)
+    elif count == [1,1,3]:
+        return (4,hand)
+    elif count == [1,2,2]:
+        return (3,hand)
+    elif count == [1,1,1,2]:
+        return (2,hand)
+    elif count == [1,1,1,1,1]:
+        return (1,hand)
 
 file = open("day7/input.txt", "r")
 text = file.read()
-hands = {}
-for line in text.split('\n'):
-    l = line.split(' ')
-    hands[l[0]] = int(l[1]) 
-handValues ={}
-
-for hand in hands.keys():
-    cards ={}
-    for card in hand:
-        cards[card] = cards.get(card,0)+1
-    validCards = []
-    validCards.append(greatestHand(cards,5))
-    validCards.append(greatestHand(cards,4))
-    validCards.append(greatestHand(cards,3) and greatestHand(cards,2))
-    validCards.append(greatestHand(cards,3))
-    validCards.append(greatestHand(cards,2,True))
-    validCards.append(greatestHand(cards,2))
-    validCards.append(greatestHand(cards,1))
-    score = len(validCards) - validCards.index(True)
-    handValues[hand] = calculateValue(hand,score)
-
-i = len(hands)
-total = 0
-while i > 0:
-    hand = max(handValues, key=handValues.get)
-    #print(i,hand,hands[hand])
-    total += hands[hand] * i
-    i-=1
-    handValues[hand] = -1
-print(total)
+hands = [line.split(" ") for line in text.split('\n')]
+hands = [handScore(hand) for hand in hands]
+hands = sorted(hands, key=lambda element: (element[0], cardValue(element[1][0])))
+score = sum([(i+1)*int(x[1][1]) for i,x in enumerate(hands)])
+print(score)
