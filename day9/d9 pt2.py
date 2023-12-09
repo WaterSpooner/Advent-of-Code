a0c7f85d5ffcd46408cb44f2b72dfe34ee5ecd1c
Max Spooner <max@the-spooners.co.uk>
2023-12-09 16:28:58 +0000

    day 9 complete

---
+++ day9/d9 pt2.py
@@ -0,0 +1,15 @@
+file = open("day9/input.txt", "r")
+values = [[int(number) for number in line.split(" ")] for line in file.read().split("\n")]
+totalCount = 0
+for value in values:
+    difference = value
+    count = 0
+    i = 0
+    while difference.count(0) != len(difference):
+        oldDifference = difference.copy()
+        count += difference[0] if i % 2 == 0 else  difference[0]*-1
+        difference = []
+        for i in range(len(oldDifference)-1):
+            difference.append(oldDifference[i+1]-oldDifference[i])
+    totalCount += count
+print(totalCount)
